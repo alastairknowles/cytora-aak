79efd1f55207cd3254c7d6b63cae112c5e94491b
alastair knowles <realalknowles@gmail.com>
2024-01-04 23:55:27 +0000

    Simplified deployment to use zip for zipping

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-jar -cfM ./function.zip -C ./python/ .
+zip -r -j ./function.zip ./python/*
 aws lambda update-function-code --function-name assessment-function  --zip-file fileb://./function.zip
-rm function.zip

a9c92926dde448ca6a76a465bcdeb5f6587520ca
alastair knowles <realalknowles@gmail.com>
2024-01-05 01:32:38 +0000

    Cleanup

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 zip -r -j ./function.zip ./python/*
-aws lambda update-function-code --function-name assessment-function  --zip-file fileb://./function.zip
+aws lambda update-function-code --function-name assessment-function --zip-file fileb://./function.zip
 
 deployed=$(curl -s -o /dev/null -w "%{http_code}" https://assessment.realalknowles.com)
 if [ "$deployed" = "200" ]
